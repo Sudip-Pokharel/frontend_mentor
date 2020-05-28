011f683f5aa39c729b0f572adf11f14715b2aca8
Stupid7 <you@example.com>
2020-05-28 10:12:26 +0545

    bookmark landing page

--- bookmark-landing-page/js/main.js
+++ bookmark-landing-page/js/main.js
@@ -1,18 +1,19 @@
 const tabLink = document.querySelectorAll('.feature__tab-link'),
-    tabContent = document.querySelectorAll('.feature__tab-content');
+    tabContent = document.querySelectorAll('.feature__tab-content'),
+    faqLink = document.querySelectorAll('a.faq__link');
 
 tabLink.forEach(function (link) {
     link.addEventListener('click', function (e) {
         e.preventDefault();
-        removeAllActiveLink();
+        removeAllActiveLink(tabLink);
         link.classList.add('active');
         removeAllActiveContent();
         document.getElementById(link.dataset.show).classList.add("active");
     })
 });
 
-function removeAllActiveLink() {
-    tabLink.forEach(function (link) {
+function removeAllActiveLink(element) {
+    element.forEach(function (link) {
         link.classList.remove('active');
     })
 }
@@ -22,3 +23,16 @@ function removeAllActiveContent() {
         content.classList.remove('active');
     })
 }
+
+faqLink.forEach(function (link) {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        if (!link.classList.contains('active')) {
+            removeAllActiveLink(faqLink);
+            link.classList.add('active');
+        }
+        else {
+            removeAllActiveLink(faqLink);
+        }
+    })
+})
