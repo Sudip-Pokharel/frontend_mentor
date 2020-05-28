0a9bc5ef35aae228b3901a9318b55f6c98cea60c
Stupid7 <you@example.com>
2020-05-28 23:32:52 +0545

    booking landing page mobile left

--- bookmark-landing-page/js/main.js
+++ bookmark-landing-page/js/main.js
@@ -1,6 +1,9 @@
 const tabLink = document.querySelectorAll('.feature__tab-link'),
     tabContent = document.querySelectorAll('.feature__tab-content'),
-    faqLink = document.querySelectorAll('a.faq__link');
+    faqLink = document.querySelectorAll('a.faq__link'),
+    inputWrapper = document.querySelector('.input__wrapper'),
+    checkEmailBtn = document.getElementById("contact__us-btn"),
+    inputEmail = document.getElementById("email__input");
 
 tabLink.forEach(function (link) {
     link.addEventListener('click', function (e) {
@@ -36,3 +39,22 @@ faqLink.forEach(function (link) {
         }
     })
 })
+
+checkEmailBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    ValidateEmail(inputEmail.value)
+})
+
+function ValidateEmail(input) {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (input.match(mailformat)) {
+        inputWrapper.classList.remove("error");
+        inputEmail.focus();
+        return true;
+    }
+    else {
+        inputWrapper.classList.add("error");
+        inputEmail.focus();
+        return false;
+    }
+}
