afbe8cad423a6e27aea0859872eaef3445661305
Stupid7 <you@example.com>
2020-06-08 10:48:30 +0545

    mobile menu left

--- manage-landing-page/js/main.js
+++ manage-landing-page/js/main.js
@@ -1,24 +1,27 @@
 const sliderBox = document.querySelector(".slider__list"),
+    slideitem = document.querySelectorAll(".slider__list-item"),
     submitBtn = document.getElementById("submitBtn"),
-    emailInput = document.getElementById("emailInput");
+    emailInput = document.getElementById("emailInput"),
+    paginationLink = document.querySelectorAll(".pagination-link");
 let sliderNumber = 2;
+
+
+submitBtn.addEventListener('click', checkEmailInput);
 
 setInterval(function () {
     removeActiveClass();
     sliderBox.classList.add(`slider__list--show-${sliderNumber}`);
     sliderNumber++;
-    if (sliderNumber == 5) {
+    if (sliderNumber == slideitem.length + 1) {
         sliderNumber = 1;
     }
-}, 5000);
+}, 5500);
 
 function removeActiveClass() {
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < slideitem.length + 1; i++) {
         sliderBox.classList.remove(`slider__list--show-${i}`);
     }
 }
-
-submitBtn.addEventListener('click', checkEmailInput);
 
 function checkEmailInput(e) {
     e.preventDefault();
@@ -27,6 +30,13 @@ function checkEmailInput(e) {
     if (emailInput.value == '' || !emailInput.value.match(mailformat)) {
         emailInput.parentElement.classList.add("error");
     }
-
 }
 
+for (let i = 0; i < paginationLink.length; i++) {
+    paginationLink[i].addEventListener('click', function (e) {
+        e.preventDefault();
+        removeActiveClass();
+        sliderBox.classList.add(`slider__list--show-${i + 1}`);
+        sliderNumber = i + 1;
+    })
+}
