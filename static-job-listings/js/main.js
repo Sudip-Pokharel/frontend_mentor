3c09709065e6c99b1e2c452ef1eced613c82b669
Stupid7 <you@example.com>
2020-06-10 22:49:56 +0545

    filtering n mobile view left

--- static-job-listings/js/main.js
+++ static-job-listings/js/main.js
@@ -25,10 +25,10 @@ Vue.component('list-item', {
             <li><a href="#" @click.prevent="$emit('filter-role',job.role)">{{job.role}}</a></li>
             <li><a href="#" @click.prevent="$emit('filter-level',job.level)">{{job.level}}</a></li>
             <li v-for="(language,index) in job.languages" :key="'language-box'+index">
-                <a href="#">{{language}}</a>
+                <a href="#" @click.prevent="$emit('filter-languages',language)">{{language}}</a>
             </li>
             <li v-for="(tool,index) in job.tools" :key="'tool-box'+index">
-                <a href="#">{{tool}}</a>
+                <a href="#" @click.prevent="$emit('filter-tools',tool)">{{tool}}</a>
             </li>
         </ul>
     </li>
@@ -36,19 +36,56 @@ Vue.component('list-item', {
     props: ['job']
 })
 
+Vue.component('filter-list', {
+    template: `
+    <li>
+        {{option}}
+        <a href="#">
+        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14">
+            <path fill="#FFF" fill-rule="evenodd"
+            d="M11.314 0l2.121 2.121-4.596 4.596 4.596 4.597-2.121 2.121-4.597-4.596-4.596 4.596L0 11.314l4.596-4.597L0 2.121 2.121 0l4.596 4.596L11.314 0z" />
+        </svg>
+        </a>
+    </li>
+    `,
+    props: ['option']
+})
+
 var app = new Vue({
     el: '#app',
     data() {
         return {
             jobs: [],
+            filter: {
+                role: '',
+                level: '',
+                languages: [],
+                tools: []
+            },
+            filterOption: false,
+            unique_key: 0
         }
     },
     methods: {
         roleFilter(data) {
-            this.jobs = this.jobs.filter(job => job.role == data);
+            this.filter.role = data;
         },
         levelFilter(data) {
-            this.jobs = this.jobs.filter(job => job.level == data);
+            this.filter.level = data;
+        },
+        languagesFilter(data) {
+            this.filter.languages.push(data);
+        },
+        toolsFilter(data) {
+            this.filter.tools.push(data);
+        },
+        clearFilter() {
+            this.filter = {
+                role: '',
+                level: '',
+                languages: [],
+                tools: []
+            }
         },
         getJobData() {
             fetch('../data.json')
@@ -57,10 +94,69 @@ var app = new Vue({
                     this.jobs = data;
                 })
                 .catch(err => console.error(err));
-        }
+        },
+
     },
     created() {
         this.getJobData();
+    },
+    computed: {
+        getFilteredJob() {
+            let checkRole = true;
+            let checkLevel = true;
+            let checkLanguages = true;
+            let checkTools = true;
+            this.filterOption = false;
+            return this.jobs.filter(job => {
+                if (this.filter.role) {
+                    this.filterOption = true;
+                    checkRole = job.role == this.filter.role;
+                }
+                if (this.filter.level) {
+                    this.filterOption = true;
+                    checkLevel = job.level == this.filter.level
+                }
+                if (this.filter.languages.length) {
+                    this.filterOption = true;
+                    if (job.languages) {
+                        let languageMatchCount = 0;
+                        job.languages.forEach(langJob => {
+
+                            this.filter.languages.forEach(langFilter => {
+                                if (langJob == langFilter) {
+                                    languageMatchCount++;
+                                }
+                            })
+
+                        })
+                        checkLanguages = languageMatchCount == this.filter.languages.length;
+                    }
+                    else {
+                        checkLanguages = false;
+                    }
+                }
+                if (this.filter.tools.length) {
+                    this.filterOption = true;
+                    if (job.tools) {
+                        let toolMatchCount = 0;
+                        job.tools.forEach(toolJob => {
+                            this.filter.tools.forEach(toolFilter => {
+                                if (toolJob == toolFilter) {
+                                    toolMatchCount++;
+                                }
+                            })
+
+                        })
+                        checkTools = toolMatchCount == this.filter.tools.length;
+                    }
+                    else {
+                        checkTools = false;
+                    }
+                }
+
+                return checkRole && checkLevel && checkLanguages && checkTools;
+            })
+        }
     }
 })
 
