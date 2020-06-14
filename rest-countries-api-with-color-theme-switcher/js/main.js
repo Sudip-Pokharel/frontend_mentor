7bc49570a705399ba92f5ada584b833841a8d50a
Stupid7 <you@example.com>
2020-06-14 12:05:30 +0545

    detail page

--- rest-countries-api-with-color-theme-switcher/js/main.js
+++ rest-countries-api-with-color-theme-switcher/js/main.js
@@ -35,10 +35,63 @@ Vue.component('country-item', {
 
 const Details = Vue.component('view-details', {
     name: 'DetailView',
-    template: "<div>view details</div>",
+    template: `
+    <div class="details">
+        <div class="container">
+            <a href="#" @click="$router.go(-1)" class="btn btn__primary back">
+                <svg id="Layer" enable-background="new 0 0 64 64" height="24" viewBox="0 0 64 64" width="33"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="m54 30h-39.899l15.278-14.552c.8-.762.831-2.028.069-2.828-.761-.799-2.027-.831-2.828-.069l-17.448 16.62c-.755.756-1.172 1.76-1.172 2.829 0 1.068.417 2.073 1.207 2.862l17.414 16.586c.387.369.883.552 1.379.552.528 0 1.056-.208 1.449-.621.762-.8.731-2.065-.069-2.827l-15.342-14.552h39.962c1.104 0 2-.896 2-2s-.896-2-2-2z" />
+                </svg>
+                Back
+            </a>
+
+            <div class="details__country" :key="'detail__wrapper'+unique_key">
+                <div class="image">
+                    <img :src="data.flag" :alt="data.name+' Flag'">
+                </div>
+                <div class="description">
+                <h3 class="title title__medium">{{data.name}}</h3>
+                <div class="content">
+                    <div>
+                    <p>Native Name: <span>{{data.nativeName}}</span> </p>
+                    <p>Population: <span>{{data.population}}</span> </p>
+                    <p>Region: <span>{{data.region}}</span> </p>
+                    <p>Sub Region: <span>{{data.subregion}}</span> </p>
+                    <p>Capital: <span>{{data.capital}}</span> </p>
+                    </div>
+                    <div>
+                    <p>Top Level Domain: <span>{{getTopLevelDomain()}}</span> </p>
+                    <p>Currencies: <span>{{getCurrencies()}}</span> </p>
+                    <p>Languages <span>{{getLanguages()}}</span> </p>
+                    </div>
+                </div>
+                <div class="border">
+                    <p> Border Countries: </p>
+                    <div class="btn-box">
+                    <router-link class="btn btn__primary" @click="unique_key+=1"  v-for="(border,index) in data.borders" :key="'border__country'+index" :to="{name:'detail_view',params:{alpha:border}}">{{border}}</router-link>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `,
     data() {
         return {
-            data: {}
+            data: {},
+            unique_key: 0
+        }
+    },
+    methods: {
+        getTopLevelDomain() {
+        },
+        getCurrencies() {
+
+        },
+        getLanguages() {
+
         }
     },
     created() {
@@ -128,10 +181,11 @@ const Home = Vue.component('list-country-box', {
 
 const routes = [
     { path: '/', name: 'home', component: Home },
-    { path: '/country/:alpha', component: Details }
+    { path: '/country/:alpha', name: 'detail_view', component: Details }
 ]
 
 const router = new VueRouter({
+    // mode: 'history',
     routes
 })
 
